cb6905b8eaa9296e59f9be78d3623d1143c8d252
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-10-31 19:51:23 +0530

    node env added in dot env

--- server/dotenv.js
+++ server/dotenv.js
@@ -3,7 +3,8 @@ import { config } from "dotenv";
 config()
 
 const appconfig = {
-    PORT : process.env.PORT,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV || 'development',
 }
 
 export default appconfig;
