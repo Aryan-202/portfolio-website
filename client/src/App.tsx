1a9d6aeb3e407475125d9d8ba191b325b4a362e2
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-11-03 20:16:55 +0530

    shadcn ui added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import './App.css'
+// import Home from './pages/Home'
+// function App() {
+//   return (
+//     <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//         </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
+
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-    </Router>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
