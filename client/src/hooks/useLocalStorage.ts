0ff5fc2ce3ed8beecc6889294abe6d5ccbbd3bdf
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2025-11-06 21:57:56 +0530

    removed coms

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
-  // Get from local storage then parse stored json or return initialValue
   const readValue = (): T => {
-    // Prevent build error "window is undefined" but keep working
     if (typeof window === 'undefined') {
       return initialValue;
     }
@@ -16,20 +14,15 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
       return initialValue;
     }
   };
-
-  // State to store our value
   const [storedValue, setStoredValue] = useState<T>(readValue);
-
-  // Return a wrapped version of useState's setter function that persists the new value to localStorage
   const setValue = (value: T | ((val: T) => T)) => {
     try {
-      // Allow value to be a function so we have the same API as useState
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       
-      // Save state
+      
       setStoredValue(valueToStore);
       
-      // Save to local storage
+     
       if (typeof window !== 'undefined') {
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
       }
@@ -40,7 +33,7 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
 
   useEffect(() => {
     setStoredValue(readValue());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    
   }, []);
 
   return [storedValue, setValue];
